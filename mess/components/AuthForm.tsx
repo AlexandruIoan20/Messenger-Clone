35ed1ba0ce6607af85ac4130eae12111001bc493
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-08-24 12:34:48 +0300

    Global button component

--- mess/components/AuthForm.tsx
+++ mess/components/AuthForm.tsx
@@ -2,6 +2,8 @@
 
 import { useState, useCallback } from 'react'; 
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+
+import Button from './buttons/Button';
 import Input from './inputs/Input';
 
 type Variant = 'LOGIN' | 'REGISTER'
@@ -50,7 +52,44 @@ const AuthForm = () => {
   return (
     <div className = 'mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
         <div className = 'bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10'>
-          <form className = 'space-y-6' onSubmit = { handleSubmit(onSubmit) }></form>
+          <form className = 'space-y-6' onSubmit = { handleSubmit(onSubmit) }>
+            { variant === 'REGISTER' && 
+              (
+                <Input 
+                  id = 'name'
+                  label = 'Name'
+                  type = 'text'
+                  register={ register }
+                  errors = { errors }
+                /> 
+              )
+            }
+
+              <Input 
+                id = 'email'
+                label = 'Email address'
+                type = 'email'
+                register={ register }
+                errors = { errors }
+              />      
+
+              <Input 
+                id = 'password'
+                label = 'Password'
+                type = { 'password' }
+                register={ register }
+                errors = { errors }
+              />  
+          </form>
+
+          <div>
+            <Button
+              disabled = { isLoading }
+              fullWidth
+              type = 'submit'> 
+              { variant === 'LOGIN' ? "Sign in" : 'Register'}
+            </Button>
+          </div>
         </div>
       </div>
   )
