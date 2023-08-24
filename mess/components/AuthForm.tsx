65dfb1bc2955782f8143fab0d965ea4058a5a163
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-08-24 11:54:57 +0300

    placeholder functions for login form

--- mess/components/AuthForm.tsx
+++ mess/components/AuthForm.tsx
@@ -1,8 +1,53 @@
-'use client'
+'use client'; 
+
+import { useState, useCallback } from 'react'; 
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+
+type Variant = 'LOGIN' | 'REGISTER'
+
 
 const AuthForm = () => {
+  const [ variant, setVariant ] = useState <Variant> ('LOGIN'); 
+  const [ isLoading, setIsLoading ] = useState <boolean> (false); 
+
+  const toggleVariant = useCallback( () => { 
+    if(variant ===  'LOGIN') 
+      setVariant('REGISTER'); 
+    else 
+      setVariant('REGISTER')
+  }, [variant]); 
+
+  const {
+    register, 
+    handleSubmit, 
+    formState: { errors }
+  } = useForm<FieldValues>({ 
+    defaultValues: { 
+      name: "", 
+      email: "", 
+      password: "", 
+    }
+  })
+
+  const onSubmit: SubmitHandler <FieldValues> = (data) => { 
+    setIsLoading(true); 
+
+    if(variant === 'REGISTER') { 
+      //fetch register
+    }
+
+    if(variant === 'LOGIN') { 
+      //NEXT AUTH sign in
+    }
+  }
+
+  const socialAction = (action: string) => { 
+    setIsLoading(true); 
+
+    //Next Auth Social Sign In
+  }
   return (
-    <div>AuthForm</div>
+    <div>AuthFo2rm</div>
   )
 }
 
