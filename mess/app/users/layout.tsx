287b6d750e3a678e153e351f43767ecfad1b96d6
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-08-30 11:24:25 +0300

    conversations route POST

--- mess/app/users/layout.tsx
+++ mess/app/users/layout.tsx
@@ -1,11 +1,16 @@
 import React from 'react'; 
-import Sidebar from '@components/sidebar/Sidebar';
+import getUsers from '@app/actions/getUsers';
 
-const UsersLayout = ({ children }: { children: React.ReactNode}) => {
+import Sidebar from '@components/sidebar/Sidebar';
+import UsersList from '@components/users/UsersList';
+
+const UsersLayout = async ({ children }: { children: React.ReactNode}) => {
+  const users = await getUsers(); 
   return (
     // @ts-expect-error Server Component
     <Sidebar>
         <div className = 'h-full'>
+          <UsersList items = { users } />
             { children }
         </div>
     </Sidebar>
---
+++ mess/components/users/UserBox.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import Avatar from '@components/Avatar';
+import { User } from '@prisma/client'; 
+import { useRouter } from 'next/navigation';
+import { useCallback, useState } from 'react';
+
+interface UserBoxProps { 
+    data: User
+}
+
+const UserBox = ( { data }: UserBoxProps) => {
+    const router = useRouter(); 
+    const [ isLoading, setIsLoading ] = useState <boolean> (false); 
+
+    const handleClick = useCallback(() => { 
+        setIsLoading(true); 
+
+        fetch('/api/conversations', { 
+            method: "POST", 
+            mode: 'cors', 
+            body: JSON.stringify({ 
+                userId: data.id
+            }), 
+            headers: { 
+                'Content-Type': "application/json"
+            }
+        }).then((data) => { 
+            console.log({ data }); 
+        }).finally(() => { setIsLoading(false)}); 
+    }, [data, router]); 
+
+    
+  return (
+    <div
+        onClick = { handleClick}
+        className = 'w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer'
+    >
+        <Avatar user = { data } />
+        <div className = 'min-w-0  flex-1'>
+            <div className = 'focus:outline-none'>
+                <div className = 'flex justify-between items-center mb-1'>
+                    <p className = 'text-sm font-medium text-gray-900'>
+                        { data.name }
+                    </p>
+                </div>
+            </div>
+        </div> 
+    </div>
+  )
+}
+
+export default UserBox
