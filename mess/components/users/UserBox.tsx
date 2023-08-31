13dfa8c8361e8ce2ae6c088f3a8ba7b50338bc15
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-08-31 18:28:18 +0300

    Loading Modal

--- mess/components/users/UserBox.tsx
+++ mess/components/users/UserBox.tsx
@@ -4,6 +4,7 @@ import Avatar from '@components/Avatar';
 import { User } from '@prisma/client'; 
 import { useRouter } from 'next/navigation';
 import { useCallback, useState } from 'react';
+import LoadingModal from '@components/LoadingModal';
 
 interface UserBoxProps { 
     data: User
@@ -32,21 +33,26 @@ const UserBox = ( { data }: UserBoxProps) => {
 
     
   return (
-    <div
-        onClick = { handleClick}
-        className = 'w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer'
-    >
-        <Avatar user = { data } />
-        <div className = 'min-w-0  flex-1'>
-            <div className = 'focus:outline-none'>
-                <div className = 'flex justify-between items-center mb-1'>
-                    <p className = 'text-sm font-medium text-gray-900'>
-                        { data.name }
-                    </p>
+    <>
+        { isLoading && 
+            <LoadingModal /> 
+        }
+        <div
+            onClick = { handleClick}
+            className = 'w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer'
+        >
+            <Avatar user = { data } />
+            <div className = 'min-w-0  flex-1'>
+                <div className = 'focus:outline-none'>
+                    <div className = 'flex justify-between items-center mb-1'>
+                        <p className = 'text-sm font-medium text-gray-900'>
+                            { data.name }
+                        </p>
+                    </div>
                 </div>
-            </div>
-        </div> 
-    </div>
+            </div> 
+        </div>
+    </>
   )
 }
 
