ce9c57a927b4eb7a86fb1d69eb46a04c60e9dc87
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-09-01 10:32:03 +0300

    fix 2

--- mess/app/users/layout.tsx
+++ mess/app/users/layout.tsx
@@ -7,7 +7,6 @@ import UsersList from '@components/users/UsersList';
 const UsersLayout = async ({ children }: { children: React.ReactNode}) => {
   const users = await getUsers(); 
   return (
-    // @ts-expect-error Server Component
     <Sidebar>
         <div className = 'h-full'>
           <UsersList items = { users } />
